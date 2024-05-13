25c2f010d99816aa4b5c43f5ceea5541ea3127d4
hridayroy19 <hrhridoyroy503@gmail.com>
2024-05-13 18:42:28 +0600

    crat a payment cart api

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 6001;
+const stripe = require("stripe")(process.env.STRIPE_SECRITE_KEY);
 
 // medilware
 app.use(cors());
@@ -231,6 +232,29 @@ async function run() {
       const result = await userCollections.deleteOne(query);
       res.send(result);
     });
+
+
+
+
+    app.post("/create-payment", async (req, res) => {
+      const {price} = req.body;
+      const amount = price*100
+
+      // Create a PaymentIntent with the order amount and currency
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+      payment_method_types: ['card']
+
+      });
+    
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+
+
+
 
     await client.db("admin").command({ ping: 1 });
     console.log(
